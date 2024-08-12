250f79849e76f8f23ee9882393069f7c7517072a
Sanmit0804 <sanmitsuthar3@gmail.com>
2024-08-13 00:40:00 +0530

    Works on StoreContext.jsx

--- Frontend/src/context/StoreContext.jsx
+++ Frontend/src/context/StoreContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null);
@@ -18,8 +18,16 @@ const StoreContextProvider = (props) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId - 1] }));
   };
 
+  useEffect(()=>{
+    console.log(cartItems);
+  }, [cartItems])
+
   const ContextValue = {
     food_list,
+    cartItems, 
+    setCartItems,
+    addToCart,
+    removeFromCart
   };
   return (
     <StoreContext.Provider value={ContextValue}>
