b8b20b7ab0e7aa0ba5ae4f85de58f1469bc83515
Sanmit0804 <sanmitsuthar3@gmail.com>
2024-08-29 19:30:19 +0530

    Working on place order feature and stripe payment integration

---
+++ Backend/controllers/orderController.js
@@ -0,0 +1,10 @@
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/orderModel.js";
+import stripe from "stripe";
+
+// Placing user order from content
+const placeOrder = async (req, res) => {
+    
+};
+
+export { placeOrder };
--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,9 @@ import cors from "cors"; // CORS -Cross-origin resource sharing
 import connectDB from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
 import userRouter from "./routes/userRoute.js";
+import "dotenv/config";
 import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 
 // app config
 const app = express();
@@ -21,6 +23,7 @@ app.use("/api/food", foodRouter);
 app.use("/images", express.static("uploads"));
 app.use("/api/user", userRouter);
 app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
   res.send("API Working!");
