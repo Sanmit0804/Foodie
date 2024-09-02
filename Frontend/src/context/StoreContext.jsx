9df8d9d09db4604276845ed7c9b325d354d43dd4
Sanmit0804 <sanmitsuthar3@gmail.com>
2024-09-02 13:19:04 +0530

    Add the actual url from render.com

--- Frontend/src/context/StoreContext.jsx
+++ Frontend/src/context/StoreContext.jsx
@@ -9,7 +9,9 @@ const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
-  const url = "http://localhost:4000";
+  
+  const url = "https://foodie-backend-j2uh.onrender.com/";
+  // const url = "http://localhost:4000";
 
   const addToCart = async (itemId) => {
     setCartItems((prev) => {
