465d3929b53c2d083cd62890cff3aa7a90a3e78e
Sanmit0804 <sanmitsuthar3@gmail.com>
2024-09-02 14:47:37 +0530

    Update url in admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-  const url = "https://foodie-backend-j2uh.onrender.com/";
+  const url = "https://foodie-backend-j2uh.onrender.com";
 
   return (
     <div>
